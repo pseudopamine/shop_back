9011ab51c1c26b5d11587affd5255b04822572c5
pseudopamine <jinseonglae@gmail.com>
2025-03-24 17:15:27 +0900

    도서 이미지 등록 ì¿기능

--- src/main/java/com/green/book_shop/book/controller/BookController.java
+++ src/main/java/com/green/book_shop/book/controller/BookController.java
@@ -21,12 +21,19 @@ public class BookController {
 
   //책 정보를 등록하는 기능 API
   @PostMapping("")
-  public void insertBook(@RequestBody BookDTO bookDTO,
-                         @RequestParam(name = "firstFile", required = false) MultipartFile file){
-    //첨부파일(도서이미지) : 단일 첨부 업로드
-    uploadUtil.fileUpload(file);
+  public void insertBook(BookDTO bookDTO,
+                         @RequestParam(name = "mainImg", required = true) MultipartFile mainImg,
+                         @RequestParam(name = "subImg", required = true) MultipartFile subImg
+                         ){
+    //첨부파일(도서이미지) : 단일 첨부 업로드 (파일 두개 받아옴)
+    uploadUtil.fileUpload(mainImg);
+    uploadUtil.fileUpload(subImg);
+
     //BOOK 테이블에 데이터 INSERT
     bookService.insertBook(bookDTO);
+
+    //BOOK_IMG 테이블에 도서 이미지 정보 INSERT
+
   }
 
   //JOIN한 모든 카테고리 정보를 불러오는 기능 API
